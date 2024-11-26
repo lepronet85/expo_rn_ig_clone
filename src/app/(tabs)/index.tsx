66ed31d4428991f733f51411dae286c841751f9b
Abdoul <lepronet85@gmail.com>
2024-11-26 11:15:12 +0100

    Setup Nativewind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const About = () => {
-  return <Text>Feed</Text>;
+const Feed = () => {
+  return (
+    <View className="bg-slate-600 items-center justify-center flex-1 m-10">
+      <Text className="text-5xl font-bold text-blue-500">Feed</Text>
+    </View>
+  );
 };
 
-export default About;
+export default Feed;
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Stack } from "expo-router";
 
+import "../../global.css";
+
 const RootLayout = () => {
   return (
     <Stack
