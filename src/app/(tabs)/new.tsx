cd0ef81b2d6d7f64d6e6038411d68aae7470b15a
Abdoul <lepronet85@gmail.com>
2024-11-26 10:29:01 +0100

    Rename Screens componenents

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const New = () => {
+const CreatePost = () => {
   return (
     <View>
       <Text>New</Text>
@@ -9,4 +9,4 @@ const New = () => {
   );
 };
 
-export default New;
+export default CreatePost;
