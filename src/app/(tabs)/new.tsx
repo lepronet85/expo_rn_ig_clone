6d5957661e5f7293c0f72f0a54411b5026e2e0dd
Abdoul <lepronet85@gmail.com>
2024-11-27 09:48:25 +0100

    Done with Create new post screen design

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,10 +1,37 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, TextInput, Pressable } from "react-native";
+import React, { useEffect } from "react";
 
 const CreatePost = () => {
+  const [caption, setCaption] = React.useState<string>("");
+
   return (
-    <View>
-      <Text>New</Text>
+    <View className="p-3 items-center flex-1">
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+        }}
+        className="w-52 aspect-[3/4] rounded-lg shadow-md"
+      />
+      <Text
+        onPress={() => console.log("press")}
+        className="text-blue-500 font-semibold my-5"
+      >
+        Change
+      </Text>
+      <TextInput
+        value={caption}
+        onChangeText={setCaption}
+        placeholder="What is on your mind"
+        className="w-full p-3 "
+      />
+      <View className="mt-auto w-full">
+        <Pressable
+          onPress={() => console.log("Pressed")}
+          className="bg-blue-500 w-full p-3 items-center rounded-md"
+        >
+          <Text className="text-white font-semibold">Share</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
